eb93b3377748b7040969fd3af780d9073cfb7365
zamedmar0710-ux <zamedmar0710@gmail.com>
2026-08-29 18:13:16 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
-# sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Tiktodz/local_manifest --depth 1 -b 19.1 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+#!/bin/bash
+MANIFEST_URL="https://github.com"
+BRANCH="lineage-22.1"
+DEVICE="a52q"
 
-# build rom
-. build/envsetup.sh
-lunch lineage_X00TD-userdebug
-export TZ=Asia/Jakarta
-#export ALLOW_MISSING_DEPENDENCIES=true
-#export SELINUX_IGNORE_NEVERALLOWS=true
-mka bacon
-# end
+# Setup paths
+mkdir -p ~/.bin
+PATH=~/.bin:$PATH
+curl https://googleapis.com > ~/.bin/repo
+chmod a+x ~/.bin/repo
+
+# Sync ROM
+repo init -u $MANIFEST_URL -b $BRANCH --depth=1
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+# Build command
+source build/envsetup.sh
+lunch axion_a52q-userdebug
+mka bacon -j$(nproc --all)
